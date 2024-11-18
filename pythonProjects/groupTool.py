4b47378567ae60a73a44b25150f57d758fe6a7d8
AJ Middleton <11019728@uvu.edu>
2024-11-18 11:36:27 -0700

    Control Tool Update

--- pythonProjects/groupTool.py
+++ pythonProjects/groupTool.py
@@ -3,6 +3,7 @@ import maya.cmds as cmds
 selList = cmds.ls(selection=True)
 currentIndex = 0
 
+#Creates group for the current control and matches transform and rotation
 def group_Selection(control):
     conRot = cmds.xform(control, worldSpace=True, rotation=True, q=True)
     conTrans = cmds.xform(control, worldSpace=True, translation=True, q=True)
@@ -10,10 +11,12 @@ def group_Selection(control):
     cmds.xform(group, rotation=conRot, translation=conTrans)
     cmds.parent(control, group)
 
+#Creates Control circle and moves it to the position of selected object
 def create_Control(sel):
     objRot = cmds.xform(sel, worldSpace=True, rotation=True, q=True)
     objTrans = cmds.xform(sel, worldSpace=True, translation=True, q=True)
 
+    #Check for _Jnt and _Geo suffix and swaps with _Ctrl
     if "_Jnt" in sel or "_Geo" in sel:
         txt = sel
         txt2 = (txt.rpartition('_')[0])
@@ -27,7 +30,9 @@ def create_Control(sel):
     cmds.select(currentControl)
     group_Selection(currentControl)
 
-
+#Loops through all selected objects
 for i in selList:
     create_Control(selList[currentIndex])
     currentIndex += 1
+
+#To use: select objects in maya and run code
